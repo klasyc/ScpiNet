cd7adfb344354f94529c840be4ce0fc7c5c76cc1
vojtech <vojtech@ozm.cz>
2024-09-21 18:03:19 +0200

    Add sample application for USB devices.

---
+++ SampleApp/MyScpiDevice.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Logging;
+using ScpiNet;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+	/// <summary>
+	/// This class represents a simplistic device driver for some SCPI device.
+	/// </summary>
+	public class MyScpiDevice : ScpiDevice
+	{
+		/// <summary>
+		/// This static factory method is used to asynchronously connect the device, handle its initial connection tasks
+		/// and finally return the instance of device driver.
+		/// </summary>
+		/// <param name="connection">Connection instance to use for communication.</param>
+		/// <param name="logger">Optional logger instance.</param>
+		/// <returns>Instance of connected device or exception, if connection fails.</returns>
+		public static async Task<MyScpiDevice> Create(IScpiConnection connection, ILogger<ScpiDevice> logger = null, CancellationToken cancellationToken = default)
+		{
+			// Try to open the connection:
+			logger?.LogInformation($"Opening USB connection for device {connection.DevicePath}...");
+			await connection.Open(cancellationToken);
+
+			// Get device ID:
+			logger?.LogInformation("Connection succeeded, trying to read device ID...");
+
+			string id = await connection.GetId(cancellationToken);
+			logger?.LogInformation($"Connection succeeded. Device id: {id}");
+
+			// Create the driver instance.
+			return new MyScpiDevice(connection, id, logger);
+		}
+
+		/// <summary>
+		/// The constructor is private, because we want to make the programmer to use the asynchronous factory method
+		/// (constructors cannot be async).
+		/// </summary>
+		/// <param name="connection">Instance of connection to be used for communication.</param>
+		/// <param name="deviceId">Devie ID.</param>
+		/// <param name="logger">Logger instance.</param>
+		protected MyScpiDevice(IScpiConnection connection, string deviceId, ILogger<ScpiDevice> logger = null)
+			: base(connection, deviceId, logger)
+		{
+		}
+
+		/// <summary>
+		/// This is how we can implement a driver-specific method.
+		/// </summary>
+		/// <param name="cancellationToken">Cancellation token.</param>
+		/// <returns>Value of the device's status buffer.</returns>
+		public async Task<string> ReadStatusByte(CancellationToken cancellationToken = default)
+		{
+			return await Query("*STB?", false, cancellationToken);
+		}
+	}
+}
---
+++ SampleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using ScpiNet;
+
+namespace SampleApp
+{
+	/// <summary>
+	/// This simple program demonstrates, how to control a USB TMC device with Scpi.NET.
+	/// </summary>
+	public static class Program
+	{
+		/// <summary>
+		/// Main application entry point.
+		/// </summary>
+		public static void Main()
+		{
+			// Run the asynchronous code synchronously:
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		/// <summary>
+		/// Asynchronous Main.
+		/// </summary>
+		private static async Task MainAsync()
+		{
+			List<string> devices;
+
+			// Prepare logger factory which will provide a logger instance for our driver:
+			using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+			ILogger log = loggerFactory.CreateLogger("Program");
+
+			try {
+				// List available USB devices. We will get back USB identifiers which can be used to open the device.
+				log.LogInformation("Searching USB SCPI devices...");
+				devices = UsbScpiConnection.GetUsbDeviceList();
+
+				// Exit if there is no USB device:
+				if (devices.Count == 0) {
+					log.LogError("No USB device found. Exiting.");
+					return;
+				}
+
+				// Print all device descriptors:
+				log.LogInformation("Search succeeded, found the following devices:");
+				foreach (string d in devices) {
+					Console.WriteLine(d);
+				}
+			} catch (Exception ex) {
+				log.LogError($"USB device search failed: {ex.Message}");
+				return;
+			}
+
+			// Try to connect the first available device and get its identifier:
+			try {
+				// Create the connection instance. The constructor only remembers the connection parameters,
+				// actual connection is done in connection.Open() method which is called later from the driver's factory method.
+				using IScpiConnection connection = new UsbScpiConnection(devices[0]);
+
+				// Try to connect our device using the custom device driver:
+				MyScpiDevice device = await MyScpiDevice.Create(connection, loggerFactory.CreateLogger<MyScpiDevice>());
+
+				// Once the device is ready, we can call driver-specific methods like this one:
+				string stb = await device.ReadStatusByte();
+				Console.WriteLine($"Status buffer: {stb}");
+			} catch (Exception ex) {
+				log.LogError($"Connection failed: {ex.Message}");
+			}
+		}
+	}
+}
